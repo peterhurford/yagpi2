5cba98d004c6ddf9baa865cf2f67197bc9369978
Peter Hurford <peter@peterhurford.com>
2015-11-17 17:57:40 -0600

    Test param validation

--- lib/api.rb
+++ lib/api.rb
@@ -1,6 +1,16 @@
 class Api
+  @@raises = false
+
   def self.receive_ping
     { "status" => "ping_received" }
+  end
+
+  def self.set_raises_flag!
+    @@raises = true
+  end
+
+  def self.unset_raises_flag!
+    @@raises = false
   end
 
   def self.halt!(*response)
@@ -8,53 +18,63 @@ class Api
   end
 
   def self.error!(error_message, error_type)
-    halt!(error_type,
-      {'Content-Type' => 'application/json'}, {
-        error: error_type,
-        message: error_message
-      }.to_json)
+    if @@raises
+      raise StandardError.new(error_message)
+    else
+      halt!(error_type,
+        {'Content-Type' => 'application/json'}, {
+          error: error_type,
+          message: error_message
+        }.to_json)
+    end
+  end
+
+  def self.validate_payload(payload)
+    validated_payload = {
+      github_body: payload["body"],
+      github_branch: payload["head"]["ref"],
+      github_action: payload["action"],
+      github_pr_url: payload["html_url"],
+      github_author: payload["user"]["login"]
+    }
+    error!("No action", 500) unless validated_payload[:github_action].present?
+    error!("No branch", 500) unless validated_payload[:github_branch].present?
+    error!("No PR URL", 500) unless validated_payload[:github_pr_url].present?
+    error!("No author", 500) unless validated_payload[:github_author].present?
+    validated_payload
   end
 
   def self.receive_hook_and_return_data!(params)
     return(receive_ping.to_json) if Github.is_github_ping?(params)
 
     #TODO: Mirror issues
-    github_payload = params["pull_request"]
-    error!('No payload', 500) unless github_payload.present?
+    payload = params["pull_request"]
+    error!('No payload', 500) unless payload.present?
 
-    #TODO: Clean code
-    github_body = github_payload['body']
-    github_branch = github_payload['head']['ref']
-    github_action = params['action']
-    github_pr_url = github_payload['html_url']
-    github_author = github_payload['user']['login']
-    error!('No action', 500) unless github_action.present?
-    error!('No branch', 500) unless github_branch.present?
-    error!('No PR URL', 500) unless github_pr_url.present?
-    error!('No author', 500) unless github_author.present?
+    payload = validate_payload(payload)
 
-    pivotal_id = find_pivotal_id(github_body, github_branch)
+    pivotal_id = find_pivotal_id(payload["github_body"], github_branch)
     
     yagpi_action_taken = "none"
-    if %w(opened reopened closed).include?(github_action)
+    if %w(opened reopened closed).include?(payload["github_action"])
       if pivotal_id.present?
-        if %w(opened reopened).include?(github_action)
-          change_story_state!(pivotal_id, github_pr_url, github_author, 'finished')
+        if %w(opened reopened).include?(payload["github_action"])
+          change_story_state!(pivotal_id, payload["github_pr_url"], github_author, 'finished')
           yagpi_action_taken = "finish"
-        elsif github_action == "closed"
-          change_story_state!(pivotal_id, github_pr_url, github_author, 'delivered')
+        elsif payload["github_action"] == "closed"
+          change_story_state!(pivotal_id, payload["github_pr_url"], github_author, 'delivered')
           yagpi_action_taken = "deliver"
         end
-      elsif github_action != "closed" 
-        o = nag_for_a_pivotal_id!(github_pr_url)
+      elsif payload["github_action"] != "closed" 
+        o = nag_for_a_pivotal_id!(payload["github_pr_url"])
         yagpi_action_taken = o ? "nag" : "nag disabled"
       end
     end
     
     {
-      detected_github_action: github_action,
+      detected_github_action: payload["github_action"],
       detected_pivotal_id: pivotal_id,
-      detected_github_pr_url: github_pr_url,
+      detected_github_pr_url: payload["github_pr_url"],
       detected_github_author: github_author,
       pivotal_action: yagpi_action_taken
     }
--- test/test_all.rb
+++ test/test_all.rb
@@ -3,12 +3,36 @@ require "rack/test"
 require "pry"
 require File.expand_path "../../app.rb", __FILE__
 
+
 class TestTest < Minitest::Test
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
   end
+
+  def assert_error(exception, message, &block)
+    block.call
+  rescue exception => e
+    assert_match(message, e.message)
+  end
+
+  def with_errors(&block)
+    Api.set_raises_flag!
+    block.call
+    Api.unset_raises_flag!
+  end
+
+  def complete_params
+    {
+      "body" => "test",
+      "head" => { "ref" => "test" },
+      "action" => "test",
+      "html_url" => "test",
+      "user" => { "login" => "test" }
+    }
+  end
+
 
   def test_ping
     get "/ping"
@@ -26,6 +50,19 @@ class TestTest < Minitest::Test
     post "/github_hook"
     refute(last_response.ok?)
     assert_equal(500, JSON.parse(last_response.body)["error"])
+  end
+
+  def test_that_params_validate
+    assert(Api.validate_payload(complete_params).is_a?(Hash))
+  end
+
+  def test_params_with_no_action_does_not_validate
+    with_errors do
+      no_action_params = complete_params.tap do |params|
+        params["action"] = nil
+      end
+      assert_error(StandardError, "No action") { Api.validate_payload(no_action_params) }
+    end
   end
 end
 
